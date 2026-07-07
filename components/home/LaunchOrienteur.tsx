e115ed7b36724820925f888b9d45f32484f29599
anaisalt <anais.altun@pathtech.fr>
2026-07-07 17:26:02 +0200

    feat(homepage): finish first banner

--- components/home/LaunchOrienteur.tsx
+++ components/home/LaunchOrienteur.tsx
@@ -4,13 +4,22 @@ import LaunchOrienteurCard from "./LaunchOrienteurCard";
 export default function LaunchOrienteur() {
   return (
     <section className={styles.hero}>
-      <picture className={styles.picture}>
-        <source media="(min-width: 768px)" srcSet="https://placehold.co/1440x580" />
-        <img src="https://placehold.co/375x256" alt="" className={styles.heroImage} />
-        {/* <source media="(min-width: 768px)" srcSet="/images/hero-1440x580.jpg" /> */}
-        {/* <img src="/images/hero-375x256.jpg" alt="" className={styles.heroImage} /> */}
-      </picture>
-      <LaunchOrienteurCard />
+      <div className={styles.firstGrid}>
+        <h1 className={styles.title}>
+          La santé mentale des étudiants,{" "}
+          <span className={styles.highlight}>parlons-en.</span>
+        </h1>
+        <picture className={styles.picture}>
+          <img
+            src="/images/illustrations/students-support.svg"
+            alt=""
+            className={styles.heroImage}
+          />
+        </picture>
+      </div>
+      <div className={styles.secondGrid}>
+        <LaunchOrienteurCard />
+      </div>
     </section>
   );
 }
