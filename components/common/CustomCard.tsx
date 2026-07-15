21997fbf19fb304c41e458f6e976baee98d615bb
tristan-gueguen <tristan.gueguen@gmail.com>
2026-07-15 17:30:08 +0200

    feat(ui): custom card component

---
+++ components/common/CustomCard.tsx
@@ -0,0 +1,65 @@
+import Image, { StaticImageData } from "next/image";
+import styles from "./CustomCard.module.scss";
+import { Fragment, ReactElement, ReactNode } from "react";
+import { BadgeProps } from "@codegouvfr/react-dsfr/Badge";
+
+type Props = {
+  title: string;
+  subtitle: string;
+  description: string;
+  image: StaticImageData | string;
+  imageAlt?: string;
+  button: ReactNode;
+  badges: ReactElement<BadgeProps>[];
+  location?: string;
+  openingHours?: string;
+};
+export default function CustomCard({
+  title,
+  subtitle,
+  description,
+  image,
+  imageAlt = "",
+  button,
+  badges,
+  location = "",
+  openingHours = "",
+}: Props) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <div>
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.subtitle}>{subtitle}</p>
+        </div>
+        <Image src={image} alt={imageAlt} className={styles.logo} width={87} height={64} />
+      </div>
+      <p className={styles.description}>{description}</p>
+      <div className={styles.footer}>
+        <div>
+          <div className={styles.badges}>
+            {badges.map((badge, index) => (
+              <Fragment key={index}>{badge}</Fragment>
+            ))}
+          </div>
+          {(location || openingHours) && (
+            <div className={styles.infos}>
+              {location && (
+                <span>
+                  <span className="fr-icon--sm fr-icon-map-pin-2-line"></span>
+                  {location}
+                </span>
+              )}
+              {openingHours && (
+                <span>
+                  <span className="fr-icon--sm fr-icon-time-line"></span> {openingHours}
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+        <div className={styles.button}>{button}</div>
+      </div>
+    </div>
+  );
+}
