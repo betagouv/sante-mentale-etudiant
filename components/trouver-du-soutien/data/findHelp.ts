ef2e0d45eaab9af2d6dfb58c17e1fa5e601ea1d3
tristan-gueguen <tristan.gueguen@gmail.com>
2026-07-15 18:48:52 +0200

    feat(trouver-du-soutien): fix bug

--- components/trouver-du-soutien/data/findHelp.ts
+++ components/trouver-du-soutien/data/findHelp.ts
@@ -1,59 +1,65 @@
-import { FORMAT_CAMPUS, FORMAT_CHOOSE, NEED_COUNSELING, NEED_PSY } from "@/components/orienteur/data/orienteurTree";
+import {
+  FORMAT_CAMPUS,
+  FORMAT_CHOOSE,
+  FORMAT_TALK,
+  FORMAT_WRITE,
+  NEED_COUNSELING,
+  NEED_PSY,
+} from "@/components/orienteur/data/orienteurTree";
 
 type OPTION = {
-    label: string;
-    value: string;
+  label: string;
+  value: string;
 };
 
 type MAIN_OPTION = OPTION & {
-    formatOptions: OPTION[]
-}
+  formatOptions: OPTION[];
+};
 
 type MAIN_OPTIONS = {
-    [key: string]: MAIN_OPTION
-}
+  [key: string]: MAIN_OPTION;
+};
 
 export const OPTIONS_I_NEED: MAIN_OPTIONS = {
-    whatever: {
-        label: "Peu importe",
-        value: 'whatever',
-        formatOptions: [
-        ]
-    },
-    talk: {
-        label: 'Me confier et être orienté',
-        value: NEED_COUNSELING,
-        formatOptions: [
-            {
-                label: "Tous",
-                value: 'all'
-            },
-            {
-                label: "A l'écrit",
-                value: 'write'
-            },
-            {
-                label: "A l'oral / en personne",
-                value: 'talk'
-            }
-        ]
-    },
-    psy: {
-        label: "Un suivi psy gratuit",
-        value: NEED_PSY,
-        formatOptions: [
-            {
-                label: "Tous",
-                value: 'all'
-            },
-            {
-                label: "Suivi sur un campus",
-                value: FORMAT_CAMPUS
-            },
-            {
-                label: "Choisir moi-même mon praticien",
-                value: FORMAT_CHOOSE
-            }
-        ]
-    }
-}
+  whatever: {
+    label: "Peu importe",
+    value: "whatever",
+    formatOptions: [],
+  },
+  [NEED_PSY]: {
+    label: "Me confier et être orienté",
+    value: NEED_COUNSELING,
+    formatOptions: [
+      {
+        label: "Tous",
+        value: "all",
+      },
+      {
+        label: "A l'écrit",
+        value: FORMAT_WRITE,
+      },
+      {
+        label: "A l'oral / en personne",
+        value: FORMAT_TALK,
+      },
+    ],
+  },
+  [NEED_COUNSELING]: {
+    label: "Un suivi psy gratuit",
+    value: NEED_PSY,
+    formatOptions: [
+      {
+        label: "Tous",
+        value: "all",
+      },
+      {
+        label: "Suivi sur un campus",
+        value: FORMAT_CAMPUS,
+      },
+      {
+        label: "Choisir moi-même mon praticien",
+        value: FORMAT_CHOOSE,
+      },
+    ],
+  },
+};
