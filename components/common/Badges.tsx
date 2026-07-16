02435fa0ea7113ca6033b65a9371e8e6182d1a4d
tristan-gueguen <tristan.gueguen@gmail.com>
2026-07-16 11:04:24 +0200

    feat(trouver-du-soutien): badges

---
+++ components/common/Badges.tsx
@@ -0,0 +1,33 @@
+import { Badge } from "@codegouvfr/react-dsfr/Badge";
+import styles from "./Badges.module.scss";
+
+export const BADGE_DAY = (
+  <Badge className={styles.day} key="de-jour">
+    De jour
+  </Badge>
+);
+export const BADGE_NIGHT = (
+  <Badge key="de-nuit" className={styles.night}>
+    De nuit
+  </Badge>
+);
+export const BADGE_PROFESSIONELS = (
+  <Badge key="professionels" className={styles.pro}>
+    Professionels
+  </Badge>
+);
+export const BADGE_PROFESSIONELLES = (
+  <Badge key="professionelles" className={styles.pro}>
+    Professionelles
+  </Badge>
+);
+export const BADGE_ETUDIANTS_BENEVOLES = (
+  <Badge key="etudiants-benevoles" className={styles.student}>
+    ETUDIANTS BENEVOLES
+  </Badge>
+);
+export const BADGE_H24 = (
+  <Badge key="24-24" className={styles.h24}>
+    24h/24
+  </Badge>
+);
