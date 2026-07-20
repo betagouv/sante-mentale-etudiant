772da7bd18c4e17f4f817ebf68d5a9e8a60f314a
tristan-gueguen <tristan.gueguen@gmail.com>
2026-07-20 16:09:35 +0200

    feat(header): header with logo, nom ministere, buttons

---
+++ components/layout/EmergencyModal.tsx
@@ -0,0 +1,16 @@
+import styles from "./EmergencyModal.module.scss";
+import { TILE_114, TILE_3114, TILE_SAMU } from "../common/Tiles";
+
+export function EmergencyModal() {
+  return (
+    <p>
+      En cas de danger immédiat ou de pensées suicidaires, appelle l'un de ces numéros.{" "}
+      <b>Appels gratuits, confidentiels et anonymes.</b>
+      <div className={styles.container}>
+        {TILE_3114}
+        {TILE_SAMU}
+        {TILE_114}
+      </div>
+    </p>
+  );
+}
