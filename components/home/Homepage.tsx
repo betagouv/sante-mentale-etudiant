16064880a244e9d1d066c2bc712a3382a36dcafc
anaisalt <anais.altun@pathtech.fr>
2026-08-12 17:58:34 +0200

    feat(home): second banner stats

--- components/home/Homepage.tsx
+++ components/home/Homepage.tsx
@@ -1,9 +1,11 @@
 import HomeHero from "./HomeHero";
+import HomeStudentsStat from "./HomeStudentsStat";
 
 export default function Homepage() {
   return (
     <>
       <HomeHero />
+      <HomeStudentsStat />
     </>
   );
 }
