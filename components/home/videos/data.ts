ab40b9a2aae396ffacf1833d03d7b6fa1c326cec
anaisalt <anais.altun@pathtech.fr>
2026-08-14 12:28:13 +0200

    feat: setup video testimonials structure and tmp datas

---
+++ components/home/videos/data.ts
@@ -0,0 +1,60 @@
+import type { VideoTestimonials } from "./types";
+
+export const testimonials: VideoTestimonials[] = [
+  {
+    key: "temoignage-1",
+    videoUrl: "https://sme-staging.s3.fr-par.scw.cloud/videos/test_video_longue.mp4",
+    previewUrl: "/videos/tmp/preview.mp4",
+    posterUrl: "/images/tmp/tmp_video.jpg",
+    citation: "J'ai fini par en parler",
+  },
+  {
+    key: "temoignage-2",
+    videoUrl: "https://sme-staging.s3.fr-par.scw.cloud/videos/test_video_longue.mp4",
+    previewUrl: "/videos/tmp/preview.mp4",
+    posterUrl: "/images/tmp/tmp_video.jpg",
+    citation: "Je ne suis plus seul avec ça",
+  },
+  {
+    key: "temoignage-3",
+    videoUrl: "https://sme-staging.s3.fr-par.scw.cloud/videos/test_video_longue.mp4",
+    previewUrl: "/videos/tmp/preview.mp4",
+    posterUrl: "/images/tmp/tmp_video.jpg",
+    citation: "J'ai retrouvé confiance en mes choix",
+  },
+  {
+    key: "temoignage-4",
+    videoUrl: "https://sme-staging.s3.fr-par.scw.cloud/videos/test_video_longue.mp4",
+    previewUrl: "/videos/tmp/preview.mp4",
+    posterUrl: "/images/tmp/tmp_video.jpg",
+    citation: "J'ai du mal à mettre des mots sur mes émotions",
+  },
+  {
+    key: "temoignage-5",
+    videoUrl: "https://sme-staging.s3.fr-par.scw.cloud/videos/test_video_longue.mp4",
+    previewUrl: "/videos/tmp/preview.mp4",
+    posterUrl: "/images/tmp/tmp_video.jpg",
+    citation: "J'ai fini par en parler",
+  },
+  {
+    key: "temoignage-6",
+    videoUrl: "https://sme-staging.s3.fr-par.scw.cloud/videos/test_video_longue.mp4",
+    previewUrl: "/videos/tmp/preview.mp4",
+    posterUrl: "/images/tmp/tmp_video.jpg",
+    citation: "J'ai fini par en parler",
+  },
+  {
+    key: "temoignage-7",
+    videoUrl: "https://sme-staging.s3.fr-par.scw.cloud/videos/test_video_longue.mp4",
+    previewUrl: "/videos/tmp/preview.mp4",
+    posterUrl: "/images/tmp/tmp_video.jpg",
+    citation: "J'ai fini par en parler",
+  },
+  {
+    key: "temoignage-8",
+    videoUrl: "https://sme-staging.s3.fr-par.scw.cloud/videos/test_video_longue.mp4",
+    previewUrl: "/videos/tmp/preview.mp4",
+    posterUrl: "/images/tmp/tmp_video.jpg",
+    citation: "J'ai fini par en parler",
+  },
+];
---
+++ components/home/videos/types.ts
@@ -0,0 +1,7 @@
+export interface VideoTestimonials {
+  key: string;
+  videoUrl: string;
+  previewUrl?: string;
+  posterUrl?: string;
+  citation?: string;
+}
