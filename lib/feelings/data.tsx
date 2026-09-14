d0dabfa9d99d5630053b2b77e0f588973048e811
tristan-gueguen <tristan.gueguen@gmail.com>
2026-09-14 17:17:43 +0200

    feat(feelings): + solitude

--- lib/feelings/data.tsx
+++ lib/feelings/data.tsx
@@ -1,4 +1,5 @@
 import { FEELING_ANXIETY } from "./anxiety";
 import { FEELING_FATIGUE } from "./fatigue";
+import { FEELING_SOLITUDE } from "./solitude";
 
-export const feelings = [FEELING_ANXIETY, FEELING_FATIGUE];
+export const feelings = [FEELING_ANXIETY, FEELING_FATIGUE, FEELING_SOLITUDE];
