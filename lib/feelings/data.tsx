d3b3d6d3035c1b2e3e85deeaeccff9b4084b3515
tristan-gueguen <tristan.gueguen@gmail.com>
2026-09-15 10:39:37 +0200

    feat(feelings): + painful event

--- lib/feelings/data.tsx
+++ lib/feelings/data.tsx
@@ -2,6 +2,7 @@ import { FEELING_ANXIETY } from "./anxiety";
 import { FEELING_DRUGS } from "./drugs";
 import { FEELING_EATING_DISORDER } from "./eating-disorder";
 import { FEELING_FATIGUE } from "./fatigue";
+import { FEELING_PAINFUL_EVENT } from "./painful-event";
 import { FEELING_SOLITUDE } from "./solitude";
 
 export const feelings = [
@@ -10,4 +11,5 @@ export const feelings = [
   FEELING_SOLITUDE,
   FEELING_DRUGS,
   FEELING_EATING_DISORDER,
+  FEELING_PAINFUL_EVENT,
 ];
