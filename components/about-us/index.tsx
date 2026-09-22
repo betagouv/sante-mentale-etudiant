5585b6bc50912636ec4477b2f6ef7e91e35edd4c
tristan-gueguen <tristan.gueguen@gmail.com>
2026-09-22 13:21:30 +0200

    feat(about-us): why this site

--- components/about-us/index.tsx
+++ components/about-us/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import WhoAreWe from "./WhoAreWe";
-import WhyThisSite from "./WhyThisSite";
 import WhatsOnThisSite from "./WhatsOnThisSite";
 import Experts from "./Experts";
+import NeedToTalkBanner from "../common/NeedToTalkBanner";
+import Link from "next/link";
+import Button from "@codegouvfr/react-dsfr/Button";
+import WhyThisSite from "./WhyThisSite";
 export default function AboutUs() {
   return (
     <>
@@ -10,6 +13,32 @@ export default function AboutUs() {
       <WhyThisSite />
       <WhatsOnThisSite />
       <Experts />
+      <NeedToTalkBanner
+        title={
+          <>
+            Une idée ?<br />
+            Envie de participer au projet ?
+          </>
+        }
+        subtitle="Nous avons toujours besoin d'échanger avec des étudiants."
+        hint={
+          <>
+            ou en écrivant à{" "}
+            <Link
+              href="mailto:boiteaidees@santementale.etudiant.gouv.fr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              boiteaidees@santementale.etudiant.gouv.fr
+            </Link>
+          </>
+        }
+        button={
+          <Button linkProps={{ href: "mailto:boiteaidees@santementale.etudiant.gouv.fr" }}>
+            Contacter l’équipe
+          </Button>
+        }
+      />
     </>
   );
 }
