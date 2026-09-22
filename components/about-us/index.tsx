49892053dc5bee36d53ade8eda029f9c6f8b10e6
tristan-gueguen <tristan.gueguen@gmail.com>
2026-09-22 10:21:32 +0200

    feat(about-us): structure and first section

---
+++ components/about-us/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import WhoAreWe from "./WhoAreWe";
+import WhyThisSite from "./WhyThisSite";
+import WhatsOnThisSite from "./WhatsOnThisSite";
+import Experts from "./Experts";
+export default function AboutUs() {
+  return (
+    <>
+      <WhoAreWe />
+      <WhyThisSite />
+      <WhatsOnThisSite />
+      <Experts />
+    </>
+  );
+}
