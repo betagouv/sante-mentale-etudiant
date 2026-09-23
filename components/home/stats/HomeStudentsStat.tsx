1ff632d99becdf204ec2204f1d8bad498e104f8e
tristan-gueguen <tristan.gueguen@gmail.com>
2026-09-23 17:39:56 +0200

    feat(qui-sommes-nous): footer + home link

--- components/home/stats/HomeStudentsStat.tsx
+++ components/home/stats/HomeStudentsStat.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import FullBleedSection from "../../wrapper/FullBleedSection";
 import styles from "./HomeStudentsStat.module.scss";
 
@@ -9,6 +10,9 @@ export default function HomeStudentsStat() {
       </h1>
       <p className={styles.subtitle}>
         <b>Tu n'es pas seul</b> et de nombreux dispositifs existent pour t'accompagner
+      </p>
+      <p>
+        <Link href="/qui-sommes-nous">Qui sommes-nous et pourquoi ce site ?</Link>
       </p>
     </FullBleedSection>
   );
