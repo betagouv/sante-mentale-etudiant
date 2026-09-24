e66e6f19d8eaf9d950eb3573a2f97d2d1ff8b961
tristan-gueguen <tristan.gueguen@gmail.com>
2026-09-24 16:23:29 +0200

    accessibility: title articles

--- app/s-informer/[slug]/page.tsx
+++ app/s-informer/[slug]/page.tsx
@@ -1,6 +1,17 @@
 import { notFound } from "next/navigation";
 import { getAllInternalArticleSlugs, getArticleBySlug } from "@/lib/articles";
 import Article from "@/components/articles/article/Article";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await params;
+  const article = await getArticleBySlug(slug);
+  return { title: article?.title };
+}
 
 export async function generateStaticParams() {
   return getAllInternalArticleSlugs().map((slug) => ({ slug }));
